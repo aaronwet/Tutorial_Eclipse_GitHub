2bd11b846258d61de03b62d6df9e7187d3057508
Usuario <Usuario@192.168.1.152>
2023-02-08 12:54:25 +0100

    Prueba

---
+++ Mundo/src/Hola_mundo/Hola_mundo.java
@@ -0,0 +1,16 @@
+package Hola_mundo;
+
+public class Hola_mundo {
+
+	public Hola_mundo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("¡HOLA MUNDO!");
+
+	}
+
+}
